b843e9de8694ff09feba6e79d94d0fb65c6375c8
Daniel Galvão <galvaodev@gmail.com>
2017-02-01 00:40:37 -0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+import cep from 'cep-promise';
+
+document
+    .getElementById("searchCep")
+    .addEventListener("click", () => {
+
+        let cepValue = document
+            .getElementById('cepInput')
+            .value;
+
+        cep(cepValue).then((data) => {
+            document.querySelector('.city-result').innerHTML = data.city;
+        }).catch(console.log)
+    });
+
